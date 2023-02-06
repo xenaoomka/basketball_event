9763b9d980228a8a51171503d34edcf039fea123
xenaoomka <xenaoomka@gmail.com>
2023-02-06 14:29:05 -0700

    included 12pm events

--- app.py
+++ app.py
@@ -4,39 +4,57 @@ import datetime
 app = Flask(__name__)
 events = {
     "Monday 5:30am": [],
+    "Monday 12:00pm": [],
     "Wednesday 5:30am": [],
+    "Wednesday 12:00pm": [],
     "Friday 5:30am": [],
+    "Friday 12:00pm": []
 }
 
 @app.route("/")
 def index():
     now = datetime.datetime.now()
     day_of_week = now.strftime("%A")
-    # now = datetime.datetime(2022,2,18,10,5,0)
-    # day_of_week = "Saturday"
+    #now = datetime.datetime(2022,2,28,5,5,0)
+    #day_of_week = "Tuesday"
     time = now.strftime("%I:%M%p")
 
     if day_of_week == "Saturday":
-            event = "Monday 5:30am"
             date = (now + datetime.timedelta(days=2)).strftime("%m/%d/%Y")
+            event = "Monday 5:30am"
+
     if day_of_week == "Sunday":
-            event = "Monday 5:30am"
             date = (now + datetime.timedelta(days=1)).strftime("%m/%d/%Y")
-    if day_of_week == "Monday":
             event = "Monday 5:30am"
+            
+    if day_of_week == "Monday":
             date = (now + datetime.timedelta(days=0)).strftime("%m/%d/%Y")
+            if now.hour < 6:
+                event = "Monday 5:30am"
+            else:
+                event = "Monday 12:00pm"
+
     if day_of_week == "Tuesday":
             event = "Wednesday 5:30am"
             date = (now + datetime.timedelta(days=1)).strftime("%m/%d/%Y")
+
     if day_of_week == "Wednesday":
-            event = "Wednesday 5:30am"
-            date = (now + datetime.timedelta(days=0)).strftime("%m/%d/%Y")      
+            date = (now + datetime.timedelta(days=0)).strftime("%m/%d/%Y")
+            if now.hour < 6:
+                event = "Wednesday 5:30am"
+            else:
+                event = "Wednesday 12:00pm"      
+
     if day_of_week == "Thursday":
             event = "Friday 5:30am"
             date = (now + datetime.timedelta(days=1)).strftime("%m/%d/%Y") 
+
     if day_of_week == "Friday":
-            event = "Friday 5:30am"
             date = (now + datetime.timedelta(days=0)).strftime("%m/%d/%Y") 
+            if now.hour < 6:
+                event = "Friday 5:30am"
+            else:
+                event = "Friday 12:00pm"
 
     return render_template("index.html", date=date, event=event, attendees=events[event])
 
