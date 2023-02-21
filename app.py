9648987cc83371bf389e04fb12f39715460c4856
xenaoomka <xenaoomka@gmail.com>
2023-02-21 10:12:54 -0700

    updates to include db

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect
 import datetime
+import sqlite3
 
 app = Flask(__name__)
+conn = sqlite3.connect('db/attendees.db', check_same_thread=False)
 events = {
     "Monday 5:30am": [],
     "Monday 12:00pm": [],
@@ -19,57 +21,61 @@ def index():
     #day_of_week = "Tuesday"
     time = now.strftime("%I:%M%p")
 
-    # Clear the list of attendees for each event
-    for event in events:
-        events[event] = []
-
     if day_of_week == "Saturday":
             date = (now + datetime.timedelta(days=2)).strftime("%m/%d/%Y")
-            event = "Monday 5:30am"
+            event = date+"Monday 5:30am"
 
     if day_of_week == "Sunday":
             date = (now + datetime.timedelta(days=1)).strftime("%m/%d/%Y")
-            event = "Monday 5:30am"
+            event = date+" Monday 5:30am"
             
     if day_of_week == "Monday":
             date = (now + datetime.timedelta(days=0)).strftime("%m/%d/%Y")
             if now.hour < 6:
-                event = "Monday 5:30am"
+                event = date+" Monday 5:30am"
             else:
-                event = "Monday 12:00pm"
+                event = date+" Monday 12:00pm"
 
     if day_of_week == "Tuesday":
-            event = "Wednesday 5:30am"
-            date = (now + datetime.timedelta(days=1)).strftime("%m/%d/%Y")
+            date = (now + datetime.timedelta(days=1)).strftime("%m/%d/%Y")  
+            event = date+" Wednesday 5:30am"
+            
 
     if day_of_week == "Wednesday":
             date = (now + datetime.timedelta(days=0)).strftime("%m/%d/%Y")
             if now.hour < 6:
-                event = "Wednesday 5:30am"
+                event = date+" Wednesday 5:30am"
             else:
-                event = "Wednesday 12:00pm"      
+                event = date+" Wednesday 12:00pm"      
 
     if day_of_week == "Thursday":
-            event = "Friday 5:30am"
             date = (now + datetime.timedelta(days=1)).strftime("%m/%d/%Y") 
-
+            event = date+" Friday 5:30am"
+ 
     if day_of_week == "Friday":
             date = (now + datetime.timedelta(days=0)).strftime("%m/%d/%Y") 
             if now.hour < 6:
-                event = "Friday 5:30am"
+                event = date+" Friday 5:30am"
             else:
-                event = "Friday 12:00pm"
+                event = date+" Friday 12:00pm"
 
-    return render_template("index.html", date=date, event=event, attendees=events[event])
-
+    c = conn.cursor()
+    c.execute("SELECT name FROM attendees WHERE event=?", (event,))
+    attendees = [row[0] for row in c.fetchall()]
+    c.close()
+    return render_template("index.html", date=date, event=event, attendees=attendees)
 
 
 @app.route("/checkin", methods=["POST"])
 def checkin():
     name = request.form["name"]
     event = request.form["event"]
-    events[event].append(name)
+    c = conn.cursor()
+    c.execute("INSERT INTO attendees (name, event) VALUES (?, ?)", (name, event))
+    conn.commit()
+    c.close()
     return redirect("/")
 
 if __name__ == "__main__":
+    conn.execute("CREATE TABLE IF NOT EXISTS attendees (name TEXT, event TEXT)")
     app.run()
